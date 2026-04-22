a4721f1c9c752c54759e7cf25b0d473de2372e0a
kuol buom <kuolbuom111@gmail.com>
2026-04-22 16:05:46 +0300

    mongoose validation applied

--- phonebookDatabase/step6/models/persons.js
+++ phonebookDatabase/step6/models/persons.js
@@ -12,7 +12,11 @@ mongoose.connect(url, {family: 4})
 })
 
 const personSchema = mongoose.Schema({
-    name: String,
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
     number: String,
     date: {
         type:Date,
