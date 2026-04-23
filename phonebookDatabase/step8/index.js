d380d24fbb98bb2734a728670593be63f1cf4f30
kuol buom <kuolbuom111@gmail.com>
2026-04-23 09:48:30 +0300

    added custom validation to phonebook application

---
+++ phonebookDatabase/step8/index.js
@@ -0,0 +1,114 @@
+require('dotenv').config()
+const express = require('express');
+
+const Person = require('./models/persons.js')
+
+
+const app = express();
+app.use(express.json())
+
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next)=> {
+    Person.findById(request.params.id)
+    .then(persons => {
+        if(persons){
+          response.json(persons)
+        }else{
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+        .then(count => {
+            const date = new Date()
+            response.send(`
+                <p>Phonebook has info for ${count} people</p>
+                <p>${date}</p>
+            `)
+        })
+        .catch(error => next(error))
+})
+
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: 'name or number is missing' })
+    }
+
+    Person.findOne({ name: body.name })
+    .then(existingPerson => {
+        if (existingPerson) {
+            return response.status(400).json({ error: 'name must be unique' })
+        }
+
+        const person = new Person({
+            name: body.name,
+            number: body.number,
+        })
+
+        person
+        .save()
+        .then(saved => response.json(saved))
+        .catch(error => next(error))
+    })
+    
+})
+
+app.delete('/api/persons/:id', (request, response, next)=> {
+    Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (!updatedPerson) {
+        return response.status(404).end()
+      }
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running ont this port ${PORT}`)
+})
